020509510bc704b227bf7d84f66829a55cd525b8
indiff <indiff@126.com>
2025-02-10 10:21:08 +0800

    fix.c:/nginx-module-vts/src/ngx_http_vhost_traffic_status_dump.c(396): error C2065: 'SSIZE_MAX': undeclared identifier

--- src/ngx_http_vhost_traffic_status_dump.c
+++ src/ngx_http_vhost_traffic_status_dump.c
@@ -6,8 +6,7 @@
 
 #include "ngx_http_vhost_traffic_status_module.h"
 #include "ngx_http_vhost_traffic_status_dump.h"
-
-
+#include <limits.h>
 static u_char  NGX_HTTP_VHOST_TRAFFIC_STATUS_DUMP_DATA_PAD[] = { 0x53, 0x54, 0x56 };
 
 
